d7e08a6485e5d553bd220cc127276769f9d614a0
MynnTapp <Desmynnmjc@gmail.com>
2024-12-09 15:46:38 -0600

    test

--- backend/db/migrations/20240801220558-create-spot.js
+++ backend/db/migrations/20240801220558-create-spot.js
@@ -76,6 +76,6 @@ module.exports = {
    },
    async down(queryInterface, Sequelize) {
       options.tableName = "Spots";
-      return queryInterface.dropTable("Spots",options);
+      return queryInterface.dropTable(options);
    },
 };
