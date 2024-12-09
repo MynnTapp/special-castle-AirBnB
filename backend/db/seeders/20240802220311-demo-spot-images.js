9910dd7d8597f8faa3af579f2d749dacc33b7e6e
MynnTapp <Desmynnmjc@gmail.com>
2024-12-09 09:06:51 -0600

    finished maybe

--- backend/db/seeders/20240802220311-demo-spot-images.js
+++ backend/db/seeders/20240802220311-demo-spot-images.js
@@ -22,8 +22,23 @@ module.exports = {
          [
             {
                spotId: 1,
-               url: "https://media.istockphoto.com/id/506903162/photo/luxurious-villa-with-pool.jpg?s=612x612&w=0&k=20&c=Ek2P0DQ9nHQero4m9mdDyCVMVq3TLnXigxNPcZbgX2E=",
+               url: "https://cdn.openart.ai/published/9ohAD2ktCjGkZWAOLxle/le4zwnzr_BIsd_1024.webp",
                preview: true,
+            },
+            {
+               spotId: 1,
+               url: "https://town-n-country-living.com/wp-content/uploads/2022/12/Little-Pink-Cottage.jpg",
+               preview: false,
+            },
+            {
+               spotId: 1,
+               url: "https://www.bostonmagazine.com/wp-content/uploads/sites/2/2023/06/the-pink-house-3.jpg",
+               preview: false,
+            },
+            {
+               spotId: 1,
+               url: "https://media.gettyimages.com/id/160808926/photo/victorian-house.jpg?s=612x612&w=gi&k=20&c=8ZM_KxhH0fL3bZpT9aZZDovCviZ2KsWVp6ORBC5Ihec=",
+               preview: false,
             },
             {
                spotId: 1,
@@ -31,73 +46,58 @@ module.exports = {
                preview: false,
             },
             {
-               spotId: 1,
-               url: "https://media.istockphoto.com/id/506903162/photo/luxurious-villa-with-pool.jpg?s=612x612&w=0&k=20&c=Ek2P0DQ9nHQero4m9mdDyCVMVq3TLnXigxNPcZbgX2E=",
-               preview: false,
-            },
-            {
-               spotId: 1,
-               url: "https://media.istockphoto.com/id/506903162/photo/luxurious-villa-with-pool.jpg?s=612x612&w=0&k=20&c=Ek2P0DQ9nHQero4m9mdDyCVMVq3TLnXigxNPcZbgX2E=",
-               preview: false,
-            },
-            {
-               spotId: 1,
-               url: "https://media.istockphoto.com/id/506903162/photo/luxurious-villa-with-pool.jpg?s=612x612&w=0&k=20&c=Ek2P0DQ9nHQero4m9mdDyCVMVq3TLnXigxNPcZbgX2E=",
-               preview: false,
-            },
-            {
                spotId: 2,
-               url: "https://www.travelandleisure.com/thmb/UvM2xJbeXQT6x3gLkapyM0mTJSM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/winchester-new-ROOM1016-2ce0d52ad0724d60b6885654132402b8.jpg",
+               url: "https://cdn.openart.ai/published/9ohAD2ktCjGkZWAOLxle/ouSstbsp_nj-R_1024.webp",
                preview: true,
             },
             {
                spotId: 2,
-               url: "https://www.travelandleisure.com/thmb/UvM2xJbeXQT6x3gLkapyM0mTJSM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/winchester-new-ROOM1016-2ce0d52ad0724d60b6885654132402b8.jpg",
+               url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUgrFiau_yCvGGOtn1FKe_r1oJtjaPyyx2Cw&s",
                preview: false,
             },
             {
                spotId: 2,
-               url: "https://www.travelandleisure.com/thmb/UvM2xJbeXQT6x3gLkapyM0mTJSM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/winchester-new-ROOM1016-2ce0d52ad0724d60b6885654132402b8.jpg",
+               url: "https://i.pinimg.com/236x/de/60/bf/de60bfa6eed6e1f1bdd78fbc39fc66ae.jpg",
                preview: false,
             },
             {
                spotId: 2,
-               url: "https://www.travelandleisure.com/thmb/UvM2xJbeXQT6x3gLkapyM0mTJSM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/winchester-new-ROOM1016-2ce0d52ad0724d60b6885654132402b8.jpg",
+               url: "https://media.kvue.com/assets/KVUE/images/3a7d2b77-d815-44f3-8f1f-202cf4b2079f/3a7d2b77-d815-44f3-8f1f-202cf4b2079f_1140x641.png",
                preview: false,
             },
             {
                spotId: 2,
-               url: "https://www.travelandleisure.com/thmb/UvM2xJbeXQT6x3gLkapyM0mTJSM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/winchester-new-ROOM1016-2ce0d52ad0724d60b6885654132402b8.jpg",
+               url: "https://thumbs.dreamstime.com/z/pink-floral-cute-house-spring-garden-color-scheme-generative-ai-beautiful-fairy-271606203.jpg",
                preview: false,
             },
             {
                spotId: 3,
-               url: "https://www.historichotels.org/images/uploads/hhapush/Haunted/Concords-Colonial-Inn.jpg",
+               url: "https://cdnfiles.hdrcreme.com/24399/medium/Pink-House.jpg?1426829653",
                preview: true,
             },
             {
                spotId: 3,
-               url: "https://www.historichotels.org/images/uploads/hhapush/Haunted/Concords-Colonial-Inn.jpg",
+               url: "https://eastbrookhomes.com/wp-content/uploads/2023/03/484348639-exterior-1024x683.jpg",
                preview: false,
             },
             {
                spotId: 3,
-               url: "https://www.historichotels.org/images/uploads/hhapush/Haunted/Concords-Colonial-Inn.jpg",
+               url: "https://t3.ftcdn.net/jpg/08/82/90/96/360_F_882909679_bEd7AClh8dCXYfzPlXHs8vmh4vzUstB2.jpg",
                preview: false,
             },
             {
                spotId: 3,
-               url: "https://www.historichotels.org/images/uploads/hhapush/Haunted/Concords-Colonial-Inn.jpg",
+               url: "https://i.pinimg.com/originals/bf/d0/d4/bfd0d4e0d12deb11f1889a1650a11ab1.jpg",
                preview: false,
             },
             {
                spotId: 3,
-               url: "https://www.historichotels.org/images/uploads/hhapush/Haunted/Concords-Colonial-Inn.jpg",
+               url: "https://64.media.tumblr.com/38ecdf482ff0629f425dc8deb0ac54d9/0dbc7ba12e10e2f5-77/s640x960/a54671e07a1d118ae4524d6011fb52fe58be642f.jpg",
                preview: false,
             },
             {
                spotId: 4,
-               url: "https://www.historichotels.org/images/uploads/Inspiration/2023_Haunted/600x600_Omni_Homestead_Resort_Haunted_.png",
+               url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeVpgz-QAKFpHqmZLsIGTssy51vj_UCSxtNw&s",
                preview: true,
             },
             {
@@ -117,17 +117,17 @@ module.exports = {
             },
             {
                spotId: 4,
-               url: "https://www.historichotels.org/images/uploads/Inspiration/2023_Haunted/600x600_Omni_Homestead_Resort_Haunted_.png",
+               url: "https://stpetecatalyst.com/wp-content/uploads/2022/04/02-20256-Gulf-Blvd-2-scaled.jpg",
                preview: false,
             },
             {
                spotId: 5,
-               url: "https://www.historichotels.org/images/uploads/Inspiration/2023_Haunted/600x600-The-Maryland-Inn-at-Twilight.jpg",
+               url: "https://admin.onlyinyourstate.com/wp-content/uploads/sites/2/2021/04/Screen-Shot-2021-04-09-at-11.26.14-AM.png",
                preview: true,
             },
             {
                spotId: 5,
-               url: "https://www.historichotels.org/images/uploads/Inspiration/2023_Haunted/600x600-The-Maryland-Inn-at-Twilight.jpg",
+               url: "https://savageventures.b-cdn.net/wp-content/uploads/2023/07/beach-bungalow.jpg",
                preview: false,
             },
             {
@@ -172,7 +172,7 @@ module.exports = {
             },
             {
                spotId: 7,
-               url: "https://phgcdn.com/images/uploads/ABESM/masthead/Image-of-Sayre-Mansion-Exterior-The-Sayre-Mansion-Bethlehem-Pennsylvania.png",
+               url: "https://sugarsbeach.com/wp-content/uploads/2021/07/Tybee-Island-Georgia-Beach-Cottage-Lets-Flamingle-Exterior.jpeg",
                preview: true,
             },
             {
@@ -182,12 +182,12 @@ module.exports = {
             },
             {
                spotId: 7,
-               url: "https://phgcdn.com/images/uploads/ABESM/masthead/Image-of-Sayre-Mansion-Exterior-The-Sayre-Mansion-Bethlehem-Pennsylvania.png",
+               url: "https://admin.onlyinyourstate.com/wp-content/uploads/sites/2/2021/04/Screen-Shot-2021-04-09-at-11.09.02-AM.png?w=500&quality=50",
                preview: false,
             },
             {
                spotId: 7,
-               url: "https://phgcdn.com/images/uploads/ABESM/masthead/Image-of-Sayre-Mansion-Exterior-The-Sayre-Mansion-Bethlehem-Pennsylvania.png",
+               url: "https://img.freepik.com/premium-photo/charming-pink-castle-home-magical-world-artfully-crafted-by-generative-ai_318002-2949.jpg",
                preview: false,
             },
             {
@@ -207,7 +207,7 @@ module.exports = {
             },
             {
                spotId: 8,
-               url: "https://www.historichotels.org/images/uploads/hhapush/Haunted/Hanover-Inn-Dartmouth.jpg",
+               url: "https://www.realestate.com.au/news-image/w_1280,h_720/v1658754232/news-lifestyle-content-assets/wp-content/production/capi_3be7fb23c5bf16ee8ec0803efba76c99_acdc07b49606f1bfd29b0b000d06745f.jpeg?_i=AA",
                preview: false,
             },
             {
@@ -222,7 +222,7 @@ module.exports = {
             },
             {
                spotId: 9,
-               url: "https://www.historichotels.org/images/uploads/hhapush/Haunted/Omni-Parker-House.jpg",
+               url: "https://vacasa-units.imgix.net/5568636.jpg",
                preview: true,
             },
             {
--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -4,43 +4,37 @@ import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import { createSelector } from "reselect";
 
-const spotsSelector = createSelector(
-   [(state) => state.session],
-   (session) => session.user
-);
+const spotsSelector = createSelector([(state) => state.session], (session) => session.user);
 
 export default function Navigation({ isLoaded }) {
-   const sessionUser = useSelector(spotsSelector);
-   console.log(sessionUser);
-   return (
-      <ul className="nav-box">
-         <li className="image-box">
-            <NavLink data-testid="logo" to="/">
-               <img
-                  src="../../../public/Pixel-Paradises-Icon.png"
-                  className="logo-image"
-               />
+  const sessionUser = useSelector(spotsSelector);
+  console.log(sessionUser);
+  return (
+    <ul className="nav-box">
+      <li className="image-box">
+        <NavLink data-testid="logo" to="/">
+          <img src="https://www.dbackdrop.com/cdn/shop/files/M5-151.jpg?v=1684748585" className="logo-image" />
+        </NavLink>
+      </li>
+      <li className="welcome">Welcome to Pink life of Airbnb's</li>
+      {isLoaded && (
+        <>
+          <span
+            style={{
+              display: sessionUser ? "block" : "none",
+              position: "relative",
+              left: "12.5vw",
+            }}
+          >
+            <NavLink to="/spots/new" data-testid="create-new-spot-button">
+              Create a New Spot
             </NavLink>
-         </li>
-         <li className="welcome">Welcome to Pixel Paradises</li>
-         {isLoaded && (
-            <>
-               <span
-                  style={{
-                     display: sessionUser ? "block" : "none",
-                     position: "relative",
-                     left: "12.5vw",
-                  }}
-               >
-                  <NavLink to="/spots/new" data-testid="create-new-spot-button">
-                     Create a New Spot
-                  </NavLink>
-               </span>
-               <li className="profile-box">
-                  <ProfileButton user={sessionUser} />
-               </li>
-            </>
-         )}
-      </ul>
-   );
+          </span>
+          <li className="profile-box">
+            <ProfileButton user={sessionUser} />
+          </li>
+        </>
+      )}
+    </ul>
+  );
 }
