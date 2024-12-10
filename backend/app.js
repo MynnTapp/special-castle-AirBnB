e189fbbb4d5e6b64957923b3f0af267018004fe5
MynnTapp <Desmynnmjc@gmail.com>
2024-12-10 16:29:02 -0600

    strike

--- backend/app.js
+++ backend/app.js
@@ -84,7 +84,7 @@ app.use((err, _req, res, _next) => {
    res.status(err.status || 500).json({
       message: err.message,
       errors: err.errors,
-      stack: isProduction ? undefined : err.stack,
+      //stack: isProduction ? undefined : err.stack,
    });
 });
 
