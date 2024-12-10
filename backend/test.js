c8e0e0d0b8d8991d5efbf9f9995a79364afdad52
MynnTapp <Desmynnmjc@gmail.com>
2024-12-09 20:45:39 -0600

    .env

--- backend/test.js
+++ backend/test.js
@@ -1,24 +1,4 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: process.env.DB_FILE,
-  logging: console.log
-});
+require('dotenv').config();
 
-(async () => {
-  try {
-    const tableExists = await sequelize.query(
-      "SELECT name FROM sqlite_master WHERE type='table' AND name='Spots';",
-      { type: Sequelize.QueryTypes.SELECT }
-    );
-    if (tableExists.length > 0) {
-      console.log('Table "Spots" exists.');
-    } else {
-      console.log('Table "Spots" does not exist.');
-    }
-  } catch (error) {
-    console.error('Error checking table existence:', error);
-  } finally {
-    await sequelize.close();
-  }
-})();
+
+console.log('Database file path:', process.env.DB_FILE);
